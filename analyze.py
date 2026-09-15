8b6a887b38689dae2d8488992f770aa92fb41c9d
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-09-15 23:07:31 +0530

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -7,6 +7,7 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
+
 # -- 1. Load data -------------------------------------------------------------
 df = pd.read_csv("fleet_history.csv")
 
